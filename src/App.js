e44793cb6ec20f0b0b41789b6e8454a52d47f0f6
Syed_Sibteali_Baqar <sibteali786@gmail.com>
2021-05-09 11:49:49 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          <br>Updated By Sibteali</br>
+          <br />
+          Updated By Sibteali
         </p>
         <a
           className="App-link"
